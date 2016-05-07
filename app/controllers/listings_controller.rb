2c061e16f3eae00ab6e55dd5b88f1f21c276c5f9
Winston <winston.yongwei@gmail.com>
2016-05-07 11:29:58 +0800

    User can edit and update a listing.
    Introduce the concept of partials: http://guides.rubyonrails.org/layouts_and_rendering.html#using-partials
    to keep things DRY for listing and form.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,12 +8,26 @@ class ListingsController < ApplicationController
   end
 
   def create
-    @listing = Listing.new(params.require(:listing).permit(:title, :content, :state, :zipcode, :image_url))
+    @listing = Listing.new(model_params)
 
     if @listing.save
       redirect_to @listing, notice: 'You have successfully created a Listing!'
     else
       render :new
+    end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+
+    if @listing.update(model_params)
+      redirect_to @listing, notice: 'You have successfully updated a Listing!'
+    else
+      render :edit
     end
   end
 
@@ -26,5 +40,11 @@ class ListingsController < ApplicationController
     @listing.destroy
 
     redirect_to listings_path
+  end
+
+  private
+
+  def model_params
+    params.require(:listing).permit(:title, :content, :state, :zipcode, :image_url)
   end
 end
