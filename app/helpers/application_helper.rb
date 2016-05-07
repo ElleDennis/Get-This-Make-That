4821adf6255f0eef44c10990c12bf825d47f5b8d
Winston <winston.yongwei@gmail.com>
2016-05-07 15:11:17 +0800

    Add a helper to improve on display of error messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def display_form_errors(resource)
+    if resource.errors.any?
+      content_tag(:div, class: 'alert alert-danger') do
+        html =  ''
+        html << content_tag(:p, 'Sorry. We were unable to proceed because:')
+        html << content_tag(:ul) do
+          resource.errors.full_messages.
+            map { |msg| content_tag(:li, msg) }.
+            join('')
+            .html_safe
+        end
+        html.html_safe
+      end
+    end
+  end
 end
